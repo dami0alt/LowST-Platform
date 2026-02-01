d3d94fee92bd720fb102556b6b142593a2a0fa2c
dami0alt <daltamiranoontiveros@gmail.com>
2026-02-01 19:22:42 +0100

    feat:Scrapers and target.yamlconfiguration file created

---
+++ include/scrapers/pccomponentes.py
@@ -0,0 +1,36 @@
+import logging
+from .base_scraper import Product,ScraperBase
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class PcComponentes(ScraperBase):
+    def extract_data(self, url:str) -> Product:
+        html_content = super()._get_html(url)
+        html_soup = BeautifulSoup(html_content,"lxml")
+
+        #Get price and currency
+        price_span = html_soup.select_one("#pdp-price-current-integer")
+        price_w_currency  = price_span.get_text(strip=True)
+
+        price = price_w_currency[:-1].replace(".","")
+        price = float(price.replace(",","."))
+        currency = price_w_currency[-1]
+
+        #Get name
+        name_span = html_soup.select_one("#pdp-title")
+        name = str(name_span.get_text(strip=True))
+        name = super()._normalize_text(name)
+        #Get timestamp
+        timestamp = datetime.now()
+
+        #Create Product object
+        product = Product(
+            name=name,
+            price=price,
+            currency=currency,
+            url=url,
+            timestamp=timestamp)
+        return product
