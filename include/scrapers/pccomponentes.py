41b1ebf15628432c7f84b3e0c2fc4b158c3eae2f
dami0alt <daltamiranoontiveros@gmail.com>
2026-02-11 00:55:08 +0100

    refact: New scraper version with new parameters
    I expanded the data collection adding new parameters
    to the class, successfully extracting the data.

--- include/scrapers/pccomponentes.py
+++ include/scrapers/pccomponentes.py
@@ -3,35 +3,70 @@ from .core.interface import Product
 from .core.http import HttpScraper
 from bs4 import BeautifulSoup
 from datetime import datetime
+import json
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class PcComponentes(HttpScraper):
     def extract_data(self, url:str) -> Product:
+        gtin13 = None
+        name = None
+        manufacturer = None
+        price = None
+        currency = None
+        
+        # Get html and parse to soup
         html_content = super()._get_html(url)
         html_soup = BeautifulSoup(html_content,"lxml")
 
-        #Get price and currency
+        # Find alls LD+JSON
+        scripts = html_soup.find_all("script", {"type": "application/ld+json"})
+
+        # Get GTIN13(EAN) & Manufacturer
+        if scripts:
+            for script in scripts:
+                try:
+                    data = json.loads(script.string)
+
+                    if isinstance(data, dict):
+                        if data.get("@type","").lower() == "product":
+                            gtin13 = int(data["gtin13"])
+                            brand = data.get("brand")
+                            if isinstance(brand, dict):
+                                manufacturer = brand.get("name",None).capitalize()
+                            break
+                except json.JSONDecodeError:
+                    continue
+        
+        # Get price and currency
         price_span = html_soup.select_one("#pdp-price-current-integer")
-        price_w_currency  = price_span.get_text(strip=True)
 
-        price = price_w_currency[:-1].replace(".","")
-        price = float(price.replace(",","."))
-        currency = price_w_currency[-1]
+        if price_span:
+            price_w_currency  = price_span.get_text(strip=True)
+    
+            price = price_w_currency[:-1].replace(".","")
+            price = float(price.replace(",","."))
+            currency = price_w_currency[-1]
 
-        #Get name
+        # Get name
         name_span = html_soup.select_one("#pdp-title")
-        name = str(name_span.get_text(strip=True))
-        name = super()._normalize_text(name)
+        if name_span:
+            name = str(name_span.get_text(strip=True))
+            name = super()._normalize_text(name)
+
         #Get timestamp
         timestamp = datetime.now()
 
         #Create Product object
         product = Product(
+            ean=gtin13,
             name=name,
+            manufacturer=manufacturer,
             price=price,
             currency=currency,
             url=url,
-            timestamp=timestamp)
+            timestamp=timestamp,
+            source="PcComponentes"
+        )
         return product
