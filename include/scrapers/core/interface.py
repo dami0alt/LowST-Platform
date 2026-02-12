96a45079c30157c359d9f2f2ea3c09e2e22a4be7
dami0alt <daltamiranoontiveros@gmail.com>
2026-02-12 19:11:14 +0100

    refact: Changes in "Product" dataclass
    I changed the structure adding new fields and setting
    default values.

--- include/scrapers/core/interface.py
+++ include/scrapers/core/interface.py
@@ -2,22 +2,28 @@ import logging
 from datetime import datetime 
 import unicodedata
 import re
-from dataclasses import dataclass 
+from dataclasses import dataclass, field
 from abc import ABC, abstractmethod
+from typing import Optional, Dict, Any
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 @dataclass
 class Product():
-    ean: int
-    name: str  
-    manufacturer: str
-    price: float
-    currency: str
-    url: str
+    # primary keys
+    link_id: int
     timestamp: datetime
-    source: str
+    # obligatory fields
+    name: str  
+    price: float
+    currency: str = "EUR"
+    # optional fields
+    manufacturer: Optional[str] = None
+    ean: Optional[str] =  None
+    is_refurbished: bool = False
+    store_amount: int = -1
+    raw_metadata: Dict[str, Any] = field(default_factory=dict)
 
 class ScraperBase(ABC):
     @abstractmethod  
